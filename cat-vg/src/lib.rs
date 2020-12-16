6e798d3431e4b13935877d63497c809c155bd0c4
Win <xionwin@hotmail.com>
2020-12-16 13:58:16 +0800

    Update

--- cat-vg/src/lib.rs
+++ cat-vg/src/lib.rs
@@ -1,46 +1,11 @@
-// extern crate gl_graphic;
-// extern crate graphic_core;
-
-// use core::sync::atomic::{AtomicU8, Ordering};
-// use gl_graphic::{SDLWindow};
-// use graphic_core::Renderer;
-
-// use std::thread;
-// use std::time::Duration;
-// use std::sync::Arc;
-
-
-// pub fn test() {
-//     let mut window = SDLWindow::new(0, 0, 600, 600, "Game");
-    
-// let counter = Arc::new(AtomicU8::new(0u8));
-//     let ref_rbg = counter.clone();
-//     thread::spawn(move || {
-//         // some work here
-//         loop {
-//             counter.fetch_add(1, Ordering::SeqCst);
-//             thread::sleep(Duration::from_millis(1));
-//         }
-//     });
-
-//     window.show(&|renderer| {
-//         let i = (*ref_rbg).load(Ordering::SeqCst);
-//         println!("{}", i);
-//         renderer.clear(i, 255u8, 255u8);
-//     });
-// }
-
 extern crate gl_graphic;
 extern crate graphic_core;
-
 
 use gl_graphic::{SDLWindow};
 use graphic_core::Renderer;
 
 use std::{sync::mpsc, thread};
 use std::time::Duration;
-
-
 
 pub fn test() {
     let mut window = SDLWindow::new(0, 0, 600, 600, "Game1");
@@ -51,20 +16,18 @@ pub fn test() {
         loop {
             tx.send(idx).ok();
             idx = (idx + 1u8) % 255u8;
-            thread::sleep(Duration::from_millis(2));
+            thread::sleep(Duration::from_millis(1));
         }
     });
 
     window.show(&|renderer| {
-        let mut i = 0u8;
-        while match rx.try_recv() {
-            Ok(idx) => {
-                i = idx;
-                true
-            },
-            Err(_) => false
-        } {
-        }
+        let mut value = 0u8;
+        let i = loop {
+            match rx.try_recv() {
+                Ok(idx) => {value = idx},
+                Err(_) => break value
+            }
+        };
        
         if i > 0u8 {
             println!("{}", i);
